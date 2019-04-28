15e9a8675b372f0e139cd209ff3c734b09d454ab
wangxuhe <1053066913@qq.com>
2019-04-28 21:24:47 +0800

    编码转换添加输入路径，忽略格式

--- _tool/traverseFileCode/traverseFileCode.py
+++ _tool/traverseFileCode/traverseFileCode.py
@@ -23,42 +23,43 @@ def _traversefile(filename):
     '''
     _dict = chardet.detect(byte_str)
     _dict['filepath'] = filename
-    print(_dict)
     f.close()
 
     return _dict
 
-# 遍历文件
-def analysisfile(path):
+# 遍历文件(路径，忽略编码)
+def analysisfile(path, ignorelist):
     _map = []
     filenum = 0
+    ignorenum = 0
     for root, dirs, files in os.walk(path):
         for filename in files:
             fullpath = os.path.join(root, filename)
             if os.path.isfile(fullpath):
                 _dict = _traversefile(fullpath)
-                _map.append(_dict)
-                filenum += 1
+                if _dict['encoding'] in ignorelist:
+                    ignorenum += 1
+                else:
+                    print(_dict)
+                    _map.append(_dict)
+                    filenum += 1
             else:
                 print(fullpath + ' is not exsit file!!!')
 
-    print('遍历文件数目为:'+ str(filenum))
+    print(u'遍历文件数目为:'+ str(filenum))
+    print(u'忽略文件格式文件数目为:'+ str(ignorenum))
 
     # 写入文件
-    f = open('output.txt', 'a+')
+    f = open('output.txt', 'a')
     for item in _map:
         f.write(str(item) + '\n')
     f.close()
 
 if __name__ == '__main__':
-    # 终端命令，不可运行，提示：ImportError: No module named chardet
-    '''
-    if len(sys.argv) != 1:
-        print('Error: please input source path!!!')
-        sys.exit()
-    
-    path = sys.argv[1]
-    print(path)
-    '''
-    path = 'src'
-    analysisfile(path)
+    # mac终端命令，不可运行，提示：ImportError: No module named chardet
+    print(u'请输入你要分析的目录：')
+    path = raw_input()
+    print(u'请输入你要忽略的文件编码格式(utf-8,ascii等):')
+    ignore = raw_input()
+
+    analysisfile(path,str.lower(ignore))
