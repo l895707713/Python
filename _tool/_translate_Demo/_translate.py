49e008c0aa902a8115879072f5e812c9c68c8657
wangxuhe <wangxuhe@lohogames.com>
2019-08-05 21:15:53 +0800

    修改翻译Demo，将翻译内容放置到新文件中

--- _tool/_translate_Demo/_translate.py
+++ _tool/_translate_Demo/_translate.py
@@ -26,7 +26,7 @@ def translate(content):
         result = json.loads(response.text)
         srcstr = result['translateResult'][0][0]['src']
         tgtstr = result['translateResult'][0][0]['tgt']
-        print(u"{src} -> {tgt}".format(src=srcstr, tgt=tgtstr))
+        #print(u"{src} -> {tgt}".format(src=srcstr, tgt=tgtstr))
         return tgtstr
     else:
         print(u"有道词典调用失败")
@@ -34,21 +34,43 @@ def translate(content):
 
 # 分析文本内容
 def analyzeText(content):
+    newstr = ''
     # 获取'='的位置，若为-1表示不存在
     pos = content.find('=')
     if(pos != -1):
         # 存在的话，将等号左侧内容和右侧内容分别存储
         leftstr = content[0:pos]
         rightstr = content[pos+1:-1]
-        newstr = leftstr + translate(rightstr)
-        print(newstr)
+        newstr = leftstr + ' = ' + translate(rightstr) + '\n'
+    return newstr
 
-# 读取文件
-def readFile(filepath):
+# 启动  
+def start(filepath):
+    # 读取文件
+    print(u'读取并翻译内容中...')
+    newContent = []
     with open(filepath, 'r') as f:
         for line in f:
-            content = line.strip()
-            analyzeText(content)
+            content = analyzeText(line.strip())
+            newContent.append(content)
+    
+    count = len(newContent)
+    filename = os.path.basename(filepath)
+    if newContent is None:
+        print('{name} file translate failed'.format(name=filename))
+        return
+    print(u'翻译成功，共翻译条数:' + str(count))
+
+
+    # 写入新文件中
+    print(u'生成新文件中...')
+    newfile = open('english.l', 'w')
+    newfile.write('return { \n')
+    for i in range(count):
+        newfile.write('\t' + newContent[i])
+    newfile.write('}')
+    newfile.close()
+
 
 if __name__ == '__main__':
-    readFile('chinese.l')
+    start('chinese.l')
